0539132639a8e40bb5596d90b895f341a898758d
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-05-08 13:07:50 -0300

    feat(dto): add CreateDeveloperDto with validation for name, email, and birth date

--- src/developers/dto/create-developer.dto.ts
+++ src/developers/dto/create-developer.dto.ts
@@ -1,1 +1,13 @@
-export class CreateDeveloperDto {}
+import { IsDateString, IsEmail, IsString } from 'class-validator';
+
+export class CreateDeveloperDto {
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    esmail: string;
+
+    @IsDateString()
+    dateOfBirth: string;
+
+}
