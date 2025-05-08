8e846d540a48356634f0295d7c98935c2a16c1cb
Letícia Chamarelli <leticia.chamarelli@outlook.com>
2025-05-08 15:46:49 -0300

    add constructor to inject Developer repository into service

--- src/developers/developers.service.ts
+++ src/developers/developers.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDeveloperDto } from './dto/create-developer.dto';
 import { UpdateDeveloperDto } from './dto/update-developer.dto';
+import { Repository } from 'typeorm';
+import { Developer } from './entities/developer.entity';
 
 @Injectable()
 export class DevelopersService {
-  create(createDeveloperDto: CreateDeveloperDto) {
+  constructor(
+    private readonly repository: Repository<Developer>
+  ) {}
+
+  create(dto: CreateDeveloperDto) {
     return 'This action adds a new developer';
   }
 
@@ -16,7 +22,7 @@ export class DevelopersService {
     return `This action returns a #${id} developer`;
   }
 
-  update(id: number, updateDeveloperDto: UpdateDeveloperDto) {
+  update(id: number, dto: UpdateDeveloperDto) {
     return `This action updates a #${id} developer`;
   }
 
